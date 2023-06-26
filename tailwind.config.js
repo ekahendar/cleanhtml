b35033a4c10a9ce1cec385f09b4483d09528ac73
ekahendar <hendar@gmail.com>
2023-06-26 13:16:27 +0700

    utak-atik toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,16 @@ module.exports = {
       {
         mytheme: {
           
-          "primary": "#f50076",
-                   
-          "secondary": "#8f4263",
-                   
-          "accent": "#5c7f67",
-                   
-          "neutral": "#291e00",
-                   
-          "base-100": "#e9e7e7",
-                   
+          "primary": "#641ae6",
+          "secondary": "#d926a9",
+          "accent": "#1fb2a6",
+          "neutral": "#2a323c",
+          "base-100": "#1e3a8a",
           "info": "#3abff8",
-                   
           "success": "#36d399",
-                   
           "warning": "#fbbd23",
-                   
           "error": "#f87272",
-                   },
+        },
       },
     ],
   },
